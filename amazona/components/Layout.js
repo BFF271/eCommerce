26804c21a0eb6019ddb67f948d7c3901460e53b3
tobiaswang271 <tobias.n.wang@gmail.com>
2022-05-20 15:10:30 +0100

    Layout component styles were created.

--- amazona/components/Layout.js
+++ amazona/components/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from "next/head";
+import Link from "next/link";
 
 function Layout({title, children}) {
     return (
@@ -9,15 +10,23 @@ function Layout({title, children}) {
                 <meta name="description" content="E-Commerce Website" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div>
+            <div className={'flex min-h-screen flex-col justify-between'}>
                 <header>
-                    Header
+                    <nav className={'flex h-12 justify-between items-center px-4 shadow-md'}>
+                        <Link href="/">
+                            <a className={'text-lg font-bold'}>amazona</a>
+                        </Link>
+                        <div>
+                            <Link href="/cart"><a className={'p-2'}>Cart</a></Link>
+                            <Link href="/login"><a className={'p-2'}>Login</a></Link>
+                        </div>
+                    </nav>
                 </header>
-                <main>
+                <main className={'container m-auto mt-4 px-4'}>
                     {children}
                 </main>
-                <footer>
-                    Footer
+                <footer className={'flex h-10 justify-center items-center shadow-inner'}>
+                    <p className={'font-mono'}>Copyright © 2022 Amazona</p>
                 </footer>
             </div>
         </>
